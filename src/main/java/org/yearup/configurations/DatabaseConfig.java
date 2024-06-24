feb275e9bb5091303763fb6d55b6b897d5fd7248
courtezz johnson <9gawd1996@gmail.com>
2024-06-24 17:58:20 -0400

    ss

--- src/main/java/org/yearup/configurations/DatabaseConfig.java
+++ src/main/java/org/yearup/configurations/DatabaseConfig.java
@@ -5,18 +5,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.data.mysql.MySqlCategoryDao;
+import org.yearup.data.mysql.MySqlProductDao;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig
 {
     private BasicDataSource basicDataSource;
 
+    // autowire make a bean
     @Bean
     public BasicDataSource dataSource()
     {
         return basicDataSource;
     }
 
+    @Bean
+    public ProductDao productDao(DataSource dataSource) {
+        return new MySqlProductDao(dataSource);
+    }
+    @Bean
+    public CategoryDao categoryDao(DataSource dataSource) {
+        return new MySqlCategoryDao(dataSource);
+    }
     @Autowired
     public DatabaseConfig(@Value("${datasource.url}") String url,
                           @Value("${datasource.username}") String username,
--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,8 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -11,7 +11,10 @@ import org.yearup.models.Product;
 import java.util.List;
 
 // add the annotations to make this a REST controller
+@RestController
 // add the annotation to make this controller the endpoint for the following url
+@RequestMapping("categories")
+@CrossOrigin
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
 public class CategoriesController
@@ -20,17 +23,24 @@ public class CategoriesController
     private ProductDao productDao;
 
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
+    // constructor
+    @Autowired
+   public CategoriesController(ProductDao productDao, CategoryDao categoryDao){
+        this.productDao = productDao;
+        this.categoryDao = categoryDao;
+    }
 
-    // add the appropriate annotation for a get action
+
+    @GetMapping("")
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryDao.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
+
+    @GetMapping("{category_id}")
+    public Category getById(@PathVariable int category_id)
     {
         // get the category by id
         return null;
@@ -45,16 +55,19 @@ public class CategoriesController
         return null;
     }
 
-    // add annotation to call this method for a POST action
+
     // add annotation to ensure that only an ADMIN can call this function
+    @PostMapping("")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
         return null;
     }
-
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
@@ -63,6 +76,8 @@ public class CategoriesController
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCategory(@PathVariable int id)
     {
         // delete the category by id
